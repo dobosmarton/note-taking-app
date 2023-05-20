5dbc33fe609bd6f271a489025e266a445c8dd638
Marton Dobos <martondobos92@gmail.com>
2023-05-20 22:08:16 +0200

    feat: rich text editor setup

---
+++ src/components/editor/index.tsx
@@ -0,0 +1,95 @@
+import {
+  $getRoot,
+  $getSelection,
+  type EditorThemeClasses,
+  type EditorState,
+} from "lexical";
+
+import { TRANSFORMERS } from "@lexical/markdown";
+import { HeadingNode, QuoteNode } from "@lexical/rich-text";
+import { ListNode, ListItemNode } from "@lexical/list";
+import { LinkNode } from "@lexical/link";
+import { CodeNode } from "@lexical/code";
+
+import {
+  type InitialConfigType,
+  LexicalComposer,
+} from "@lexical/react/LexicalComposer";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+import { MarkdownShortcutPlugin } from "@lexical/react/LexicalMarkdownShortcutPlugin";
+import ToolbarPlugin from "./plugins/toolbarPlugin";
+
+const EDITOR_NODES = [
+  CodeNode,
+  HeadingNode,
+  LinkNode,
+  ListNode,
+  ListItemNode,
+  QuoteNode,
+];
+
+const theme: EditorThemeClasses = {
+  root: "p-4 border-slate-500 border-2 rounded h-full min-h-[200px] focus:outline-none focus-visible:border-black",
+  link: "cursor-pointer",
+  text: {
+    bold: "font-semibold",
+    underline: "underline",
+    italic: "italic",
+    strikethrough: "line-through",
+    underlineStrikethrough: "underlined-line-through",
+  },
+};
+
+const Placeholder = () => {
+  return (
+    <div className="absolute left-[1.125rem] top-[1.125rem] opacity-50">
+      Start writing...
+    </div>
+  );
+};
+
+function onChange(editorState: EditorState) {
+  editorState.read(() => {
+    // Read the contents of the EditorState here.
+    const root = $getRoot();
+    const selection = $getSelection();
+
+    console.log(root, selection);
+  });
+}
+
+export const Editor: React.FunctionComponent = () => {
+  const initialConfig: InitialConfigType = {
+    namespace: "note-editor",
+    theme,
+    nodes: EDITOR_NODES,
+
+    onError: (err: Error) => console.log(err.message),
+  };
+
+  return (
+    <div
+      id="editor-wrapper"
+      className={
+        "prose prose-slate prose-p:my-0 prose-headings:mb-4 prose-headings:mt-2 relative"
+      }
+    >
+      <LexicalComposer initialConfig={initialConfig}>
+        <RichTextPlugin
+          contentEditable={<ContentEditable />}
+          placeholder={<Placeholder />}
+          ErrorBoundary={LexicalErrorBoundary}
+        />
+
+        <OnChangePlugin onChange={onChange} />
+        <MarkdownShortcutPlugin transformers={TRANSFORMERS} />
+        <ToolbarPlugin />
+        <HistoryPlugin />
+      </LexicalComposer>
+    </div>
+  );
+};
---
+++ src/components/ui/dropdown.tsx
@@ -0,0 +1,214 @@
+"use client";
+
+import * as React from "react";
+import {
+  Root,
+  Trigger,
+  Group,
+  Portal,
+  Sub,
+  RadioGroup,
+  SubTrigger,
+  SubContent,
+  Item,
+  CheckboxItem,
+  Content,
+  ItemIndicator,
+  Separator,
+  Label,
+  RadioItem,
+} from "@radix-ui/react-dropdown-menu";
+import { Check, ChevronRight, Circle } from "lucide-react";
+import { cn } from "~/utils/cn";
+
+const DropdownMenu = Root as React.FunctionComponent;
+
+const DropdownMenuTrigger = Trigger as React.FunctionComponent;
+
+const DropdownMenuGroup = Group as React.FunctionComponent;
+
+const DropdownMenuPortal = Portal as React.FunctionComponent;
+
+const DropdownMenuSub = Sub as React.FunctionComponent;
+
+const DropdownMenuRadioGroup = RadioGroup as React.FunctionComponent;
+
+const DropdownMenuSubTrigger = React.forwardRef<
+  React.ElementRef<typeof SubTrigger>,
+  React.ComponentPropsWithoutRef<typeof SubTrigger> & {
+    inset?: boolean;
+  }
+>(({ className, inset, children, ...props }, ref) => (
+  <SubTrigger
+    ref={ref}
+    className={cn(
+      "flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none focus:bg-accent data-[state=open]:bg-accent",
+      inset && "pl-8",
+      className as string | undefined
+    )}
+    {...props}
+  >
+    {children}
+    <ChevronRight className="ml-auto h-4 w-4" />
+  </SubTrigger>
+));
+DropdownMenuSubTrigger.displayName = (
+  SubTrigger as React.FunctionComponent
+).displayName;
+
+const DropdownMenuSubContent = React.forwardRef<
+  React.ElementRef<typeof SubContent>,
+  React.ComponentPropsWithoutRef<typeof SubContent>
+>(({ className, ...props }, ref) => (
+  <SubContent
+    ref={ref}
+    className={cn(
+      "z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md animate-in data-[side=bottom]:slide-in-from-top-1 data-[side=left]:slide-in-from-right-1 data-[side=right]:slide-in-from-left-1 data-[side=top]:slide-in-from-bottom-1",
+      className
+    )}
+    {...props}
+  />
+));
+DropdownMenuSubContent.displayName = SubContent.displayName;
+
+const DropdownMenuContent = React.forwardRef<
+  React.ElementRef<typeof Content>,
+  React.ComponentPropsWithoutRef<typeof Content>
+>(({ className, sideOffset = 4, ...props }, ref) => (
+  <Portal>
+    <Content
+      ref={ref}
+      sideOffset={sideOffset}
+      className={cn(
+        "z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover p-1 text-popover-foreground shadow-md animate-in data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+        className
+      )}
+      {...props}
+    />
+  </Portal>
+));
+DropdownMenuContent.displayName = Content.displayName;
+
+const DropdownMenuItem = React.forwardRef<
+  React.ElementRef<typeof Item>,
+  React.ComponentPropsWithoutRef<typeof Item> & {
+    inset?: boolean;
+  }
+>(({ className, inset, ...props }, ref) => (
+  <Item
+    ref={ref}
+    className={cn(
+      "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      inset && "pl-8",
+      className
+    )}
+    {...props}
+  />
+));
+DropdownMenuItem.displayName = Item.displayName;
+
+const DropdownMenuCheckboxItem = React.forwardRef<
+  React.ElementRef<typeof CheckboxItem>,
+  React.ComponentPropsWithoutRef<typeof CheckboxItem>
+>(({ className, children, checked, ...props }, ref) => (
+  <CheckboxItem
+    ref={ref}
+    className={cn(
+      "relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      className
+    )}
+    checked={checked}
+    {...props}
+  >
+    <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
+      <ItemIndicator>
+        <Check className="h-4 w-4" />
+      </ItemIndicator>
+    </span>
+    {children}
+  </CheckboxItem>
+));
+DropdownMenuCheckboxItem.displayName = CheckboxItem.displayName;
+
+const DropdownMenuRadioItem = React.forwardRef<
+  React.ElementRef<typeof RadioItem>,
+  React.ComponentPropsWithoutRef<typeof RadioItem>
+>(({ className, children, ...props }, ref) => (
+  <RadioItem
+    ref={ref}
+    className={cn(
+      "relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      className
+    )}
+    {...props}
+  >
+    <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
+      <ItemIndicator>
+        <Circle className="h-2 w-2 fill-current" />
+      </ItemIndicator>
+    </span>
+    {children}
+  </RadioItem>
+));
+DropdownMenuRadioItem.displayName = RadioItem.displayName;
+
+const DropdownMenuLabel = React.forwardRef<
+  React.ElementRef<typeof Label>,
+  React.ComponentPropsWithoutRef<typeof Label> & {
+    inset?: boolean;
+  }
+>(({ className, inset, ...props }, ref) => (
+  <Label
+    ref={ref}
+    className={cn(
+      "px-2 py-1.5 text-sm font-semibold",
+      inset && "pl-8",
+      className
+    )}
+    {...props}
+  />
+));
+DropdownMenuLabel.displayName = Label.displayName;
+
+const DropdownMenuSeparator = React.forwardRef<
+  React.ElementRef<typeof Separator>,
+  React.ComponentPropsWithoutRef<typeof Separator>
+>(({ className, ...props }, ref) => (
+  <Separator
+    ref={ref}
+    className={cn("-mx-1 my-1 h-px bg-muted", className)}
+    {...props}
+  />
+));
+DropdownMenuSeparator.displayName = Separator.displayName;
+
+const DropdownMenuShortcut = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLSpanElement>) => {
+  return (
+    <span
+      className={cn("ml-auto text-xs tracking-widest opacity-60", className)}
+      {...props}
+    />
+  );
+};
+DropdownMenuShortcut.displayName = "DropdownMenuShortcut";
+
+export {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuCheckboxItem,
+  DropdownMenuRadioItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuGroup,
+  DropdownMenuPortal,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuRadioGroup,
+};
